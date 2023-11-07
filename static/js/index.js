0643ee73c29f0b3191156d2e1cd91fb48bdc0e1c
kotameyan <kota.matsu0515@gmail.com>
2023-11-07 14:42:20 +0900

    strawscan_uiをflaskで反映させた。11/7

--- static/js/index.js
+++ static/js/index.js
@@ -1,27 +1,24 @@
-// function previewImage() {
-//     const fileInput = document.getElementById('strawberryImage');
-//     const previewContainer = document.querySelector('.upload-label');
-//     const uploadPrompt = document.getElementById('uploadPrompt');
-//     const file = fileInput.files[0];
+// 画像をアップロードしたときにそれを表示する
+function displayImage(input) {
+    const file = input.files[0];
+    if (file) {
+        const reader = new FileReader();
+        reader.onload = function(e) {
+            // テキストを非表示にし、画像を表示する
+            document.getElementById('upload-text').style.display = 'none';
+            const imgElement = document.getElementById('uploaded-image');
+            imgElement.src = e.target.result;
+            imgElement.style.display = 'block';
 
-//     if (file) {
-//         const reader = new FileReader();
-//         reader.addEventListener('load', function () {
-//             const img = new Image();
-//             img.src = reader.result;
-//             img.onload = function() {
-//                 previewContainer.style.backgroundImage = 'url(' + reader.result + ')';
-//                 uploadPrompt.style.display = 'none';
-//             }
-//         });
-//         reader.readAsDataURL(file);
-//     } else {
-//         previewContainer.style.height = '300px';
-//         uploadPrompt.style.display = 'inline';
-//     }
-// }
+            // 枠線を点線から通常の線に変更する
+            const uploadBox = document.querySelector('#upload-box');
+            uploadBox.style.border = '3px solid #000';
+        }
+        reader.readAsDataURL(file);
+    }
+}
 
-// //キャンセルボタンで画面を更新
-// document.getElementById('cancelBtn').addEventListener('click', function() {
-//     location.reload();
-// });
+// キャンセルを押したときに画面をリロードする
+function reloadPage() {
+    location.reload();
+}
